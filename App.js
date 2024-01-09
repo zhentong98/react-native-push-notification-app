f371c85bbb99804e7b4324abf84a214f60b5f8a8
Chen Tong <chentong@xover.com.my>
2024-01-09 22:39:29 +0800

    Update

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import {useEffect} from "react";
 import {Button, StyleSheet, View} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
 import * as Notifications from 'expo-notifications';
@@ -13,6 +14,29 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
+
+    useEffect(() => {
+
+        const subscription1 = Notifications.addNotificationReceivedListener((notification) => {
+            console.log("Notification received")
+            console.log(notification);
+            const userName = notification.request.content.data.userName;
+            console.log(userName);
+        });
+
+        const subscription2 = Notifications.addNotificationResponseReceivedListener((response) => {
+            console.log("Notification response received");
+            console.log(response);
+            const userName = response.notification.request.content.data.userName;
+            console.log(userName);
+        });
+
+        return () => {
+            subscription1.remove();
+            subscription2.remove();
+        };
+
+    }, []);
 
     async function scheduleNotificationHandler() {
         const status = await Notifications.getPermissionsAsync();
@@ -36,6 +60,8 @@ export default function App() {
                 seconds: 5
             }
         });
+
+        console.log("Notification scheduled");
     }
 
     return (
